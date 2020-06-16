36cb2a9150eeafce863bdd8804442fd1034d5e95
seungh0 <will.seungho@gmail.com>
2020-06-16 23:01:59 +0900

    리플렉션 주의사항 정리

--- src/main/java/me/di/ContainerService.java
+++ src/main/java/me/di/ContainerService.java
@@ -3,6 +3,13 @@ package me.di;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
+/**
+ * 리플랙션 주의사항
+ * 1. 지나친 사용은 성능 이슈를 야기할 수 있다. 반드시 필요한 경우에만 사용
+ * 2. 컴파일 타임에 확인되지 않고, 런타임 시에 발생하는 문제를 만들 가능성이 있다.
+ * 3. 접근 지시자를 무시할 수 있다.
+ */
+
 public class ContainerService {
 
 	public static <T> T getObject(Class<T> classType) {
